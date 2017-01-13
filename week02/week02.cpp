bb9e8c5f852ff750f2c83ed0dff84ce94d230395
gishido <theshum@gmail.com>
2017-01-12 21:15:37 -0800

    test1-4 code complete

--- week02/week02.cpp
+++ week02/week02.cpp
@@ -28,9 +28,9 @@ void testErrors();
 // I suggest first commenting out all of these tests, then try to use only
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
-//#define TEST2   // for testPush()
-//#define TEST3   // for testPop()
-//#define TEST4   // for testErrors()
+#define TEST2   // for testPush()
+#define TEST3   // for testPop()
+#define TEST4   // for testErrors()
 
 /**********************************************************************
  * MAIN
