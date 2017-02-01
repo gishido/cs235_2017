77e4a1d0e1f505db36e193e3806d4e4d15bf1635
gishido <theshum@gmail.com>
2017-01-31 23:38:33 -0800

    Created and update Set.h

---
+++ week05/set.h
@@ -0,0 +1,413 @@
+/***********************************************************************
+* Header:
+*    Set.h header file
+* Author:
+*    Adam Shumway
+* Summary: 
+*    Set Template Class Header File
+************************************************************************/
+
+#ifndef SET_H
+#define SET_H
+
+#include <cassert> //for asserts
+#include <iostream>
+
+//forward declaration for SetIterator
+template <class T>
+class SetIterator;
+
+//forward declaration for SetConstIterator
+template <class T>
+class SetConstIterator;
+
+/*************************************************
+* Set
+* A Set class template that holds stuff
+*************************************************/
+template <class T>
+class Set
+{
+public:
+   // default constructor : empty and kinda useless
+   Set() : numItems(0), theCapacity(0), data(NULL) {}
+
+   // copy constructor : copy it
+   Set(const Set & rhs) throw (const char *);
+   
+   // non-default constructor : pre-allocate
+   Set(int capacity) throw (const char *);
+   
+   // destructor : free everything
+   ~Set()        {if (theCapacity) delete [] data;  }
+
+   //assignment operator
+   Set<T>& operator= (const Set<T> &rhs) throw (const char *);
+
+   //bracket [] operator
+   T& operator [](int index) throw (const char *);
+   const T& operator [](int index) const throw (const char *);
+
+   // is the container currently empty
+   bool empty() const  { return numItems == 0;         }
+
+   // returns capacity count
+   int capacity() const { return theCapacity;          }
+
+   // remove all the items from the container
+   void clear()        { numItems = 0;                 }
+
+   // how many items are currently in the container?
+   int size() const    { return numItems;              }
+
+   // add an item to the container
+   void insert(const T & t) throw (const char *);
+   void push_back(const T & t) throw (const char *);
+
+   // return an iterator to the beginning of the list
+   SetIterator <T> begin() { return SetIterator<T>(data); }
+
+   // return an iterator to the end of the list
+   SetIterator <T> end() { return SetIterator<T>(data + numItems);}
+
+   // return the const iterator to the beginning of the list
+   SetConstIterator <T> cbegin() const { return SetConstIterator<T>(data); }
+
+   // return the const iterator to the end of the list
+   SetConstIterator <T> cend() const { return SetConstIterator<T>(data + numItems);}
+   
+private:
+   T * data;          // dynamically allocated array of T
+   int numItems;      // how many items are currently in the Container?
+   int theCapacity;      // how many items can I put on the Container before full?
+};
+
+/**************************************************
+ * Set ITERATOR
+ * An iterator through Container
+ *************************************************/
+ template <class T>
+ class SetIterator
+ {
+    public:
+    //default constructor
+    SetIterator() : p(NULL) {}
+
+    //initialize to direct p to some item
+    SetIterator(T * p) : p(p) {}
+
+    //copy contructor
+    SetIterator(const SetIterator & rhs) {*this = rhs;}
+
+    //assignment operator
+    SetIterator & operator= (const SetIterator & rhs)
+    {
+        this->p = rhs.p;
+        return *this;
+    }
+
+    //not equals operator
+    bool operator != (const SetIterator & rhs) const
+    {
+        return rhs.p != this->p;
+    }
+
+    //de-reference operator
+    T & operator * () {return *p;}
+
+    //prefix increment
+    SetIterator <T> & operator++ ()
+    {
+        p++;
+        return *this;
+    }
+
+    //postfix increment
+    SetIterator <T> operator++ (int postfix)
+    {
+        SetIterator tmp(*this);
+        p++;
+        return tmp;
+    }
+
+    //pre-decrement
+    SetIterator <T> & operator-- ()
+    {
+        p--;
+        return *this;
+    }
+
+    //post-decrement
+    SetIterator <T> & operator-- (int decrement)
+    {
+        SetIterator tmp(*this);
+        p--;
+        return tmp;
+    }
+
+    private:
+    T * p;
+ };
+
+ /**************************************************
+ * Set Const ITERATOR
+ * A const iterator through Container
+ *************************************************/
+ template <class T>
+ class SetConstIterator
+ {
+    public:
+    //default constructor
+    SetConstIterator() : p(NULL) {}
+
+    //initialize to direct p to some item
+    SetConstIterator(T * p) : p(p) {}
+
+    //copy contructor
+    SetConstIterator(const SetConstIterator & rhs) {*this = rhs;}
+
+    //assignment operator
+    SetConstIterator & operator= (const SetConstIterator & rhs)
+    {
+        this->p = rhs.p;
+        return *this;
+    }
+
+    //not equals operator
+    bool operator != (const SetConstIterator & rhs) const
+    {
+        return rhs.p != this->p;
+    }
+
+    //de-reference operator
+    T & operator * () {return *p;}
+
+    //prefix increment
+    SetConstIterator <T> & operator++ ()
+    {
+        p++;
+        return *this;
+    }
+
+    //postfix increment
+    SetConstIterator <T> operator++ (int postfix)
+    {
+        SetConstIterator tmp(*this);
+        p++;
+        return tmp;
+    }
+
+    //pre-decrement
+    SetConstIterator <T> & operator-- ()
+    {
+        p--;
+        return *this;
+    }
+
+    //post-decrement
+    SetConstIterator <T> & operator-- (int decrement)
+    {
+        SetConstIterator tmp(*this);
+        p--;
+        return tmp;
+    }
+
+    private:
+    T * p;
+ };
+
+/*******************************************
+ * Set :: COPY CONSTRUCTOR
+ *******************************************/
+template <class T>
+Set <T> :: Set(const Set <T> & rhs) throw (const char *)
+{
+   assert(rhs.theCapacity >= 0);
+      
+   // do nothing if there is nothing to do
+   if (rhs.theCapacity == 0)
+   {
+      theCapacity = numItems = 0;
+      data = NULL;
+      return;
+   }
+
+   // attempt to allocate
+   try
+   {
+      data = new T[rhs.theCapacity];
+   }
+   catch (std::bad_alloc)
+   {
+      throw "ERROR: Unable to allocate buffer";
+   }
+   
+   // copy over the capacity and size
+   assert(rhs.numItems >= 0 && rhs.numItems <= rhs.theCapacity);
+   theCapacity = rhs.theCapacity;
+   numItems = rhs.numItems;
+
+   // copy the items over one at a time using the assignment operator
+   for (int i = 0; i < numItems; i++)
+      data[i] = rhs.data[i];
+
+   // the rest needs to be filled with the default value for T
+   for (int i = numItems; i < theCapacity; i++)
+      data[i] = T();
+}
+
+/**********************************************
+ * Set : NON-DEFAULT CONSTRUCTOR
+ * Preallocate the container to "theCapacity"
+ **********************************************/
+template <class T>
+Set <T> :: Set(int theCapacity) throw (const char *)
+{
+   assert(theCapacity >= 0);
+   
+   // do nothing if there is nothing to do
+   if (theCapacity == 0)
+   {
+      this->theCapacity = this->numItems = 0;
+      this->data = NULL;
+      return;
+   }
+
+   // attempt to allocate
+   try
+   {
+      data = new T[theCapacity];
+   }
+   catch (std::bad_alloc)
+   {
+      throw "ERROR: Unable to allocate buffer";
+   }
+      
+   // copy over the stuff
+   this->theCapacity = theCapacity;
+   this->numItems = 0;
+
+   // initialize the container by calling the default constructor
+   for (int i = 0; i < theCapacity; i++)
+      data[i] = T();
+}
+
+/***************************************************
+ * Set :: operator=
+ **************************************************/
+ template <class T>
+ Set <T>& Set<T> :: operator=(const Set<T> &rhs) throw (const char *)
+ {
+     assert(rhs.theCapacity >= 0);
+
+     //if there's nothing to do, do nothing
+     if (rhs.theCapacity == 0)
+     {
+         this->theCapacity = this->numItems = 0;
+         this->data = NULL;
+
+         return *this;
+     }
+
+     //attempt to allocate
+     try
+     {
+         this->data = new T[rhs.theCapacity];
+     }
+     catch (std::bad_alloc)
+     {
+         throw "ERROR: unable to allocate buffer";
+     }
+
+     //copy size and capacity
+     assert(rhs.numItems >= 0 && rhs.numItems <= rhs.theCapacity);
+     this->theCapacity = rhs.theCapacity;
+     this->numItems = rhs.numItems;
+
+     //copy items
+     for (int i = 0; i < this->numItems; i++)
+        this->data[i] = rhs.data[i];
+
+    //filling remaining array with default value of T
+    for (int i = this->numItems; i < this->theCapacity; i++)
+        this->data[i] = T();
+
+    return *this;
+ }
+
+ /***************************************************
+ * Set :: operator []
+ **************************************************/
+ template <class T>
+ T& Set<T> :: operator [](int index) throw (const char *)
+ {
+     if (index < 0 || index > numItems)
+     {
+         throw "ERROR: index out of bounds";
+     }
+
+     return data[index];
+ }
+
+ template <class T>
+ const T& Set<T> :: operator [](int index) const throw (const char *)
+ {
+     if (index < 0 || index > numItems)
+     {
+         throw "ERROR: index out of bounds";
+     }
+
+     return data[index];
+ }
+
+/***************************************************
+ * Set :: INSERT
+ * Insert an item on the end of the container
+ **************************************************/
+template <class T>
+void Set <T> :: insert(const T & t) throw (const char *)
+{
+   // do we have space?
+   if (theCapacity == 0 || theCapacity == numItems)
+      throw "ERROR: Insufficient space";
+   
+   // add an item to the end
+   data[numItems++] = t;
+}
+
+/***************************************************
+ * Set :: PUSH_BACK
+ * Extends the array size prior to an insert
+ **************************************************/
+template <class T>
+void Set<T> :: push_back(const T & t) throw (const char *)
+{
+    //create a new, temp empty array
+    T* moreData;
+
+    if (numItems >= theCapacity)
+    {
+        if (theCapacity == 0)
+        {
+            //initialize size to at least 1
+            theCapacity = 1;
+        }
+        else
+        {
+            //double size
+            theCapacity *= 2;
+        }
+
+        moreData = new T[theCapacity];
+        //copy existing items to the new array
+        for (int i = 0; i < numItems; ++i)
+            moreData[i] = data[i];
+
+        delete [] data;
+        data = moreData;
+    }
+
+    insert(t);
+}
+
+#endif //SET_H
