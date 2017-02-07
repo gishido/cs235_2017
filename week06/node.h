36b8d3c89b5923f6d2216e2d36a0af02a8d9d062
gishido <theshum@gmail.com>
2017-02-06 17:15:58 -0800

    Added basics for Node.h

---
+++ week06/node.h
@@ -0,0 +1,28 @@
+/***********************************************************************
+* Header:
+*    Node.h header file
+* Author:
+*    Adam Shumway, Jenaca Willans
+* Summary: 
+*    Node Template Class Header File
+************************************************************************/
+
+#ifndef NODE_H
+#define NODE_H
+
+#include <cassert> //for asserts
+#include <iostream>
+
+using namespace std;
+
+/*************************************************
+* Node
+* A Node class template that holds stuff
+*************************************************/
+template <class T>
+class Node
+{
+    
+};
+
+#endif //NODE_H
