0e4175c1b20469952983816e8a7cfef28008b39b
gishido <theshum@gmail.com>
2017-02-08 01:59:34 -0800

    good progress... still need some work

--- week06/node.h
+++ week06/node.h
@@ -32,33 +32,63 @@ class Node
 };
 
 template <class T>
-Node<T> * copy(const Node<T> * pHead)
+Node<T> * copy(Node<T> * &pHead)
 {
+    //Node<T> * pNew = new Node<T>;
+    Node<T> * pNew = pHead;
 
+    while(pHead != NULL)
+    {
+        // cout << "debug: pHead node 1 data - "
+            // << pHead->data << endl;
+        insert(pHead->data, pNew);
+        pHead = pHead->pNext;
+    }    
+    // for (Node<T> * p = pHead; p; p=p->pNext)
+    // {
+    //     cout << "debug: copy data is - "
+    //         << pHead->data << endl;
+    //     insert(pHead->data, pNew);
+
+    // }
+
+
+
+    return pNew;
 }
 
 template <class T>
-Node<T> * insert(T data, Node<T> * pNode, bool head = false)
+Node<T> * insert(T data, Node<T> * &pNode, bool head = false)
 {
-    if(head)
+    if(head || pNode == NULL)
         addToHead(data, pNode);
+    else
+    {
+        // cout << "debug: in else of insert\n";
+        // cout << "debug: pNode data is " 
+        //     << pNode->data << endl;
+        // cout << "debug: pNode next is "
+        //     << (pNode->pNext ? "address" : "NULL")
+        //     << endl;
+        // cout << "debug: insert data is - " << data << endl;
+        //1. create a new Node
+        Node<T> * pNew = new Node<T>;
+        pNew->data = data;
+
+        //2. fix the next pointer
+        pNew->pNext = pNode->pNext;
+
+        //3. get pNode->pNext to point to new
+        //pNode->pNext = pNew;
+    }
     
-    //1. create a new Node
-    Node<T> * pNew = new Node<T>;
-    pNew->data = data;
-
-    //2. fix the next pointer
-    pNew->pNext = pNode->pNext;
-
-    //3. get pNode->pNext to point to new
-    pNode->pNext = pNew;
-
     return pNode;
 }
 
 template <class T>
 void addToHead(T data, Node<T> * & pHead)
 {
+    // cout << "debug: adding to head\n";
   //1. create a new Node
     Node<T> * pNew = new Node<T>;
     pNew->data = data;
@@ -77,17 +107,29 @@ Node<T> * find(Node<T> * pHead, T index)
 }
 
 template <class T>
-void freeData(Node<T> * pHead)
+void freeData(Node<T> * & pHead)
 {
-    for (Node<T> * p = pHead; p; p->pNext)
-        delete p;
+    //cout << "debug: we're in freeData\n";
+    while (pHead != NULL)
+    {
+        Node<T> * p = pHead->pNext;
+        delete pHead;
+        pHead = p;
+    }
+    // for (Node<T> * p = pHead; p; p = p->pNext)
+    // {
+    //     cout << "debug: trying to delete " << p->data
+    //         << endl;
+    //     delete p;
+    // }
+
     pHead = NULL;
 }
 
 template <class T>
 ostream & operator<<(ostream & out, const Node<T> * pHead)
 {
-    for (const Node<T> * p = pHead; p; p->pNext)
+    for (const Node<T> * p = pHead; p; p = p->pNext)
         out << p->data << ' ';
     return out;
 }
