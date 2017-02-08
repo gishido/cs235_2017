2c12c86bf8e859b183226253ab1fda3babf3aaa2
gishido <theshum@gmail.com>
2017-02-07 23:52:43 -0800

    initial compile

--- week06/node.h
+++ week06/node.h
@@ -17,14 +17,55 @@ using namespace std;
 
 /*************************************************
 * Node
-* A Node class template that holds stuff
+* A Node struct template that holds stuff
 *************************************************/
 template <class T>
-struct Node
+class Node
 {
-    T data;
-    Node<T> * pNext;
+    public:
+        Node() : data(NULL), pNext(NULL) {}
+        Node(const T &t) : data(t), pNext(NULL) {} 
+    
+
+        T data;
+        Node<T> * pNext;
 
 };
+
+template <class T>
+Node<T> * copy(const Node<T> * pHead)
+{
+
+}
+
+template <class T>
+Node<T> * insert(T data, Node<T> * pNode, bool head = false)
+{
+
+}
+
+template <class T>
+void addToHead(Node<T> * & pHead, T data)
+{
+
+}
+
+template <class T>
+Node<T> * find(Node<T> * pHead, T index)
+{
+
+}
+
+template <class T>
+void freeData(Node<T> * pHead)
+{
+
+}
+
+template <class T>
+ostream & operator<<(ostream & out, Node<T> * pHead)
+{
+
+}
 
 #endif //NODE_H
