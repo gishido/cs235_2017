3280fd99816265e8ed4b736012340e00f7e3c056
gishido <theshum@gmail.com>
2017-02-10 22:09:59 -0800

    updated sortinsertion comments

--- week06/sortInsertion.h
+++ week06/sortInsertion.h
@@ -5,7 +5,7 @@
  *    This will contain just the prototype for insertionSortTest(). You may
  *    want to put other class definitions here as well.
  * Author
- *    <your names here>
+ *    Adam Shumway, Jenaca Willans
  ************************************************************************/
 
 #ifndef INSERTION_SORT_H
@@ -20,14 +20,18 @@
 template <class T>
 void sortInsertion(T array[], int num)
 {
+    //create new nodes to process array
     Node<T> * unsorted = NULL;
     Node<T> * sorted = NULL;
 
+    //load array values into unsorted list
     for (int i = 0; i < num; i++)
         insert(array[i], unsorted);
     
+    //create working current node pointer
     Node<T> * current = unsorted;
 
+    //process current node and call insertSort
     while (current != NULL)
     {
         Node<T> * next = current->pNext;
@@ -37,6 +41,7 @@ void sortInsertion(T array[], int num)
 
     int i = 0;
 
+    //set new values to array
     while (sorted != NULL)
     {
         array[i] = sorted->data;
@@ -44,6 +49,7 @@ void sortInsertion(T array[], int num)
         i++;
     }
 
+    //clear the lists
     freeData(sorted);
     freeData(unsorted);
 }
@@ -51,8 +57,10 @@ void sortInsertion(T array[], int num)
 template <class T>
 void insertSort(Node<T> ** list, Node<T> * pNode)
 {
+    //create current node pointer
     Node<T> * current;
 
+    //walk node and compare values
     if (*list == NULL || (*list)->data >= pNode->data)
     {
         pNode->pNext = *list;
