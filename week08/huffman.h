672722ae4f516a707b4acf111bcc9004064d373f
gishido <theshum@gmail.com>
2017-02-27 22:35:14 -0800

    made some updates to huffman.h and added vector.h

--- week08/huffman.h
+++ week08/huffman.h
@@ -12,6 +12,8 @@
 #define HUFFMAN_H
 
 #include <string>
+#include "pair.h"
+#include "vector.h"
 
 using namespace std;
 
@@ -21,51 +23,43 @@ string huffman(string fileName);
 
 class Huffman
 {
-    public:
+  public:
 
-    /******************************************************
-     * HUFFMAN :: EXTRACT RECURSIVE
-     * From a single completed Huffman tree, extract all the
-     * data and put it in output
-     ******************************************************/
-    void extractRecursive(BinaryNode < Pair <string, float> > * tree,
-                                   string sPrefix,
-                                   const Vector < Pair <string, float > > & input,
-                                   Pair <string, string> output [])
-    {
-       // better have a tree
-       assert(tree);
-
-       // if we are a leaf, then save the data
-       if (tree->pLeft == NULL && tree->pRight == NULL)
-       {
-          for (int i = 0; i < input.size(); i++)
-             if (input(i).getFirst() == tree->data.getFirst())
-             {
-                Pair <string, string> pair(sPrefix, input(i).getFirst());
-                output[i] = pair;
-                return;
-             }
-
-          // unable to find something matching our node
-          assert(false);
-       }
-
-       // handle the left tree
-       assert(tree->pRight);
-       extractRecursive(tree->pLeft,
-                        sPrefix + "0",
-                        input,
-                        output);
-       // handle the right tree
-       assert(tree->pLeft);
-       extractRecursive(tree->pRight,
-                        sPrefix + "1",
-                        input,
-                        output);
-    }
+   
 };
 
+/******************************************************
+ * HUFFMAN :: EXTRACT RECURSIVE
+ * From a single completed Huffman tree, extract all the
+ * data and put it in output
+ ******************************************************/
+// //input is not yet identified, so this function will have to wait
+//  void extractRecursive(BinaryNode <Pair <string, float> > * tree,string sPrefix
+//         ,const Vector <Pair <string, float> > & input, Pair <string, string> output [])
+// {
+//     // better have a tree
+//     assert(tree);
+//     // if we are a leaf, then save the data
+//     if (tree->pLeft == NULL && tree->pRight == NULL)
+//         {
+//         for (int i = 0; i < input.size(); i++)
+//             if (input(i).begin() == tree->data.getFirst())
+//             {
+//                 Pair <string, string> pair(sPrefix, input(i).getFirst());
+//                 output[i] = pair;
+//                 return;
+//             }
+//         // unable to find something matching our node
+//         assert(false);
+//     }
+    
+//     // handle the left tree
+//     assert(tree->pRight);
+//     extractRecursive(tree->pLeft, sPrefix + "0", input, output);
+    
+//     // handle the right tree
+//     assert(tree->pLeft);
+//     extractRecursive(tree->pRight, sPrefix + "1", input, output);
+// }
 
-
-#endif // HUFFMAN_h
+#endif // HUFFMAN_H
