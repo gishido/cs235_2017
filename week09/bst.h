e411a922a3c76a1820baefcbc1451138e34e2a62
gishido <theshum@gmail.com>
2017-03-03 16:39:36 -0800

    continuing the work through bst.h

--- week09/bst.h
+++ week09/bst.h
@@ -83,6 +83,7 @@ class BSTIterator;
             return it;
         }
 
+    private:
         //data elements
         BinaryNode<T> * myRoot;
         int numItems;
@@ -96,8 +97,12 @@ class BSTIterator
 
     //non-default constructor
 
+    /* These will need a stack called nodes, but I haven't 
+       done this part yet, so it won't compile */
     //operator--
     BSTIterator<T> & operator-- ();
+    //operator++
+    BSTIterator<T> & operator++ ();
 
 };
 
@@ -111,6 +116,55 @@ class BSTIterator
 *************************************************/
 //I'm not sure this is correct.  I'm commenting out the iterator for now
 BSTIterator<T> & BSTIterator::operator-- ()
+{
+    // do nothing if we have nothing
+    if (nodes.top() == NULL)
+        return *this;
+
+    // if there is a left node, take it
+    if (nodes.top()->pLeft != NULL)
+    {
+        nodes.push(nodes.top()->pLeft);
+
+        // there might be more right-most children
+        while (nodes.top()->pRight)
+            nodes.push(nodes.top()->pRight);
+        return *this;
+    }
+
+    // there are no left children, the right are done
+    assert(nodes.top()->pLeft == NULL);
+    BinaryNode <T> * pSave = nodes.top();
+    nodes.pop();
+
+    // if the parent is the NULL, we are done!
+    if (NULL == nodes.top())
+        return *this;
+
+    // if we are the right-child, got to the parent.
+    if (pSave == nodes.top()->pRight)
+        return *this;
+
+    // we are the left-child, go up as long as we are the left child!
+    while (nodes.top() != NULL && pSave == nodes.top()->pLeft)
+    {
+        pSave = nodes.top();
+        nodes.pop();
+    }
+
+    return *this;
+}
+
+/**************************************************
+* BST ITERATOR :: INCREMENT PREFIX
+*     advance by one. Note that this implementation uses
+*     a stack of nodes to remember where we are. This stack
+*     is called "nodes".
+* Author:      Adam and Jenaca, modified from operator--
+* Performance: O(log n) though O(1) in the common case
+*************************************************/
+//this needs some work... it is basically a copy of operator-- right now
+BSTIterator<T> & BSTIterator::operator++ ()
 {
     // do nothing if we have nothing
     if (nodes.top() == NULL)
