df61178ab2d6c2457bdea217a425f0f9d752fcd4
gishido <theshum@gmail.com>
2017-03-10 21:09:22 -0800

    updates from jena

---
+++ week09/wordCount.cpp
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Module:
+ *    Week 10, WORD COUNT
+ *    Brother Helfrich, CS 235
+ * Author:
+ *    Adam Shumway, Jenaca Willans
+ * Summary:
+ *    This program will implement the wordCount() function
+ ************************************************************************/
+
+#include "map.h"       // for MAP
+#include "wordCount.h" // for wordCount() prototype
+using namespace std;
+
+// commented this line out for original compile.
+// need to create a Count class for the wordCount tests.
+//void readFile(Map <string, Count> & counts, const string & fileName);
+
+/*****************************************************
+ * WORD COUNT
+ * Prompt the user for a file to read, then prompt the
+ * user for words to get the count from
+ *****************************************************/
+void wordCount()
+{
+
+}
