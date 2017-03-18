5a8f09886f52164903646dfc412140732ea6780f
gishido <theshum@gmail.com>
2017-03-18 01:27:29 -0700

    insertionsort is done, test3 is done

--- week11/sortInsertion.h
+++ week11/sortInsertion.h
@@ -20,7 +20,56 @@
 template <class T>
 void sortInsertion(T array[], int num)
 {
-}
+    //declare
+    int j;
+    int high;
+    int low;
+    int mid;
+    int hop;
+    T temp;
+
+    // begin loop
+    for (int i = 1; i < num; i++)
+    {
+        temp = array[i];
+        //starting params
+        high = i;
+        low = 0;
+        hop = 1;
+
+        //set low and high using
+        do
+        {
+            //set mid and spread level
+            mid = high - hop;
+            hop *= 2;
+
+            if (array[mid] > temp)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        while (low + hop <= high);
+
+        while (low != high)
+        {
+            mid = (low + high) / 2;
+
+            if (array[mid] > temp)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        //shift values
+        for(j = i; j > low; j--)
+            array[j] = array[j-1];
+
+        //insert
+        array[j] = temp;
+    }   
+
+}                                   
 
 
 #endif // SORT_INSERTION_H
