06014858874d514c3a4b3105e40c30d6b5815a45
gishido <theshum@gmail.com>
2017-03-23 23:19:15 -0700

    compile test 1-3

--- week12/week12.cpp
+++ week12/week12.cpp
@@ -30,7 +30,7 @@ void testQuery();
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
 #define TEST2   // for testAdd()
-// #define TEST3   // for testCopy()
+#define TEST3   // for testCopy()
 // #define TEST4   // for testQuery()
 
 /**********************************************************************
