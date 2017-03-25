fcbbe6f1ace4db13a363cfc979342412f7626bfb
gishido <theshum@gmail.com>
2017-03-25 13:49:06 -0700

    interim updates to spellCheck

--- week12/spellCheck.cpp
+++ week12/spellCheck.cpp
@@ -16,6 +16,10 @@
 #include "list.h"
 using namespace std;
 
+#define DSIZE 2322 //size for dictionary array
+#define FARRAY 130 //size for file array
+#define MARRAY 5 //sixe for misspelled array
+
 /****************************************
  * S HASH
  * A hash of string
@@ -32,12 +36,21 @@ public:
    int hash(const string & value) const
    {
       int index = 0;
+      //int newSize = numBuckets + (numBuckets * 1.5);
       for(const char * p = value.c_str(); *p; p++)
       {
          index += *p;
       }
+      index %= capacity();
       assert(0 <= index && index < capacity());
-      return index % 3000;
+      return index;
+      //when calling this hash, we'll need to calculate the size
+      // before callign the hash since it will affect capacity and other 
+      // assert functions that are based on numBuckets
+      
+      //return index % newSize;
+      //old size was static 3000, but I'm calculating a newhash size, although
+      // it doesn't match capacity.  May need to think abou that.'
       
       // per the new Data Structures text p 312
       // the hash should be approximately 1.5 times
@@ -56,9 +69,16 @@ private:
  ****************************************/
 void spellCheck()
 {
+   //we should probably defind our static sizes so
+   // we can use them in our loops
+
+
    string fileName;
-   string dictionaryArray[2322];
-   string fileArray[130];
+   string dictionaryArray[DSIZE];
+   string fileArray[FARRAY];
+
+   int hashSize = DSIZE * 1.5;
+   SHash d(hashSize);
    
    cout << "What file do you want to check? ";
    cin >> fileName;
@@ -93,15 +113,27 @@ void spellCheck()
    }
 }
 
-void readFile(string dictionaryArray)
+void readFile(string fileName, SHash d)
 {
+
    ifstream fin("dictionary.txt");
    assert(!fin.fail());
-   for (int i = 0; i < 2323; i++)
+   while (!fin.eof())
    {
-      fin >> dictionaryArray[i];
+      string value;
+      fin >> value;
+      d.insert(value);
       assert(!fin.fail());
    }
+//    for (int i = 0; i < DSIZE; i++)
+//    {
+//       //insert into the hash here?
+//       //fin >> dictionaryArray[i];
+//       string value;
+//       fin >> value;
+//       d.insert(value);
+//       assert(!fin.fail());
+//    }
    fin.close();
 
 }
