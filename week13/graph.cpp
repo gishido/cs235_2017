43f763a933668bd5cb65e14fe6b0a5d4620d2007
gishido <theshum@gmail.com>
2017-03-31 13:21:16 -0700

    intermediate updates to graph.h and .cpp

---
+++ week13/graph.cpp
@@ -0,0 +1,70 @@
+/***********************************************************************
+ * Component:
+ *    Week 13, Maze
+ *    Brother Helfrich, CS 235
+ * Author:
+ *    Adam Shumway, Jenaca Willans
+ * Summary:
+ *    Graph Class, graph.cpp
+ ************************************************************************/
+
+ #include <iostream>
+ #include "graph.h"
+
+ /******************************************
+ * GRAPH: Main Constructor
+ * Allocate a 2-d array of bools
+ ******************************************/
+ Graph::Graph(int numNode)
+ {
+     assert(numNode > 0);
+     try
+     {
+         this->matrix = new bool[numNode * numNode];
+         this->numNode = numNode;
+     }
+     catch(...)
+     {
+         this->matrix = NULL;
+         this->numNode = 0;
+         throw "ERROR: unable to allocate space for the graph";
+     }
+ }
+
+ /******************************************
+ * GRAPH: findEdges
+ * find the edge and returns a set
+ ******************************************/
+Set<Vertex> Graph::findEdges(const Vertex &vFrom)
+{
+
+}
+ 
+ /******************************************
+ * GRAPH: findPath
+ * finds a path and returns a vertex
+ ******************************************/
+Vertex<Vertex> Graph::findPath(const Vertex &vFrom, const Vertex &vTo)
+{
+
+}
+
+ /******************************************
+ * GRAPH: add
+ * Adds an edge, accepting two vertices
+ ******************************************/
+void add(const Vertex &vFrom, const Vertex &vTo)
+{
+
+}
+
+ /******************************************
+ * GRAPH: add
+ * Add and edge, accept one vertex and a set
+ *   of vertices
+ ******************************************/
+void add(const Vertex &vFrom, const Set<Vertex> &vTo)
+{
+
+}
+ 
--- week13/graph.h
+++ week13/graph.h
@@ -5,19 +5,56 @@
  * Author:
  *    Adam Shumway, Jenaca Willans
  * Summary:
- *    Graph Class
+ *    Graph Class, graph.h
  ************************************************************************/
 
 #ifndef GRAPH_H
 #define GRAPH_H
 
 #include "vector.h"
+#include "set.h"
+#include "vertex.h"
 
 //Create Graph Class
-template <class T> //not sure if we need data here, but I'm sure we will, somewhere
 class Graph
 {
+    public:
+        Graph(int numNode) {}
 
+        Graph(const Graphy &rhs) : matrix(NULL) { *this = rhs; }
+
+        ~Graph() { clear(); }
+
+        Graph &operator=(const Graph &rhs)
+        {
+            numNode = rhs.numNode;
+        }
+
+        int size() { return numNode; }
+
+        void clear () 
+        {
+            if (matrix != NULL)
+                delete [] matrix;
+        }
+
+        bool isEdge(const Vertex &vFrom, const Vertex &vTo)
+        {
+            bool theEdge = false;
+
+            return theEdge;
+        }
+
+        Set<Vertex> findEdges(const Vertex &vFrom);
+    
+        Vertex<Vertex> findPath(const Vertex &vFrom, const Vertex &vTo);
+
+        void add(const Vertex &vFrom, const Vertex &vTo);
+        void add(const Vertex &vFrom, const Set<Vertex> &vTo);
+
+    private:
+        bool * matrix;
+        int numNode;
 };
 
 #endif // GRAPH_H
