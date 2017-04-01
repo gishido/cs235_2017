157ac0d3166103b633a337af04c876f8eaecfd23
gishido <theshum@gmail.com>
2017-03-31 21:37:43 -0700

    jena update to graph.h

--- week13/graph.h
+++ week13/graph.h
@@ -18,53 +18,67 @@
 //Create Graph Class
 class Graph
 {
-    public:
-        Graph(int numNode);
+  public:
+   Graph(int numNode);
+   
+  Graph(const Graph &rhs) : matrix(NULL) { *this = rhs; }
+   
+//   Graph(const Graph &rhs) : matrix(NULL)
+//   {
+   //numNode = rhs.numNode;
+//      edges = rhs.edges;
+   //matrix = rhs.matrix;
+   //}
+   
+   ~Graph() { clear(); }
+   
+   Graph &operator=(const Graph &rhs)
+      {
+         numNode = rhs.numNode;
+      }
+   
+   // number of edges
+   int size() const { return numNode; }
+   
+   // number of possible edges
+   int capacity() const { return numNode * numNode; }
+   
+   bool empty() const { return size() == 0; }
+   
+   void clear () 
+   {
+      if (matrix != NULL)
+         delete [] matrix;
+   }
+   
+   bool isEdge(const Vertex &vFrom, const Vertex &vTo) const
+   {
+//    bool theEdge = false;
+//    return theEdge;
 
-        Graph(const Graph &rhs) : matrix(NULL) { *this = rhs; }
-
-        ~Graph() { clear(); }
-
-        Graph &operator=(const Graph &rhs)
-        {
-            numNode = rhs.numNode;
-        }
-
-        int size() const { return numNode; }
-
-        void clear () 
-        {
-            if (matrix != NULL)
-                delete [] matrix;
-        }
-
-        bool isEdge(const Vertex &vFrom, const Vertex &vTo) const
-        {
-            bool theEdge = false;
-            if (matrix[index(vFrom, vTo)] == true)
-                theEdge = true;
-
-            cout << "debug: matrix index - " << index(vFrom, vTo) << endl;
-            cout << "debug: isEdge - " << theEdge ? "true\n" : "false\n";
-
-            return theEdge;
-        }
-
-        Set<Vertex> findEdges(const Vertex &vFrom);
-    
-        Vector<Vertex> findPath(const Vertex &vFrom, const Vertex &vTo);
-
-        void add(const Vertex &vFrom, const Vertex &vTo);
-        void add(const Vertex &vFrom, const Set<Vertex> &vTo);
-
-    private:
-        int index(const Vertex &vFrom, const Vertex &vTo) const
-        {
-            assert(vFrom.index() * numNode + vTo.index() < numNode * numNode);
-            return vFrom.index() * numNode + vTo.index();
-        }
-        bool * matrix;
-        int numNode;
+      return matrix[index(vFrom, vTo)];
+      
+   }
+   
+   Set<Vertex> findEdges(const Vertex &vFrom);
+   
+   Vector<Vertex> findPath(const Vertex &vFrom, const Vertex &vTo);
+   
+   void add(const Vertex &vFrom, const Vertex &vTo);
+//             void add(const Vertex &vFrom, const Set<Vertex> &vTo);
+   void add(const Vertex &vFrom, const Set<Vertex> &sTo);
+   
+  private:
+   
+   // find the index into the array of nodes for a given edge
+   int index(const Vertex & vFrom, const Vertex & vTo) const
+   {
+      assert(vFrom.index() * numNode + vTo.index() < numNode * numNode);
+      return vFrom.index() * numNode + vTo.index();
+   }
+   
+   bool * matrix;
+   int numNode;
 };
 
 #endif // GRAPH_H
